fb4d459b3784b267429614cca89872c12ba9e946
Deiziane <deiziane_ns@hotmail.com>
2019-09-18 11:22:50 -0300

    Create clouds.py

---
+++ clouds.py
@@ -0,0 +1,23 @@
+# c = [0, 0, 1, 0, 0, 1, 0]
+
+# c = [0, 0, 0, 0, 1, 0]
+
+c = [0, 0, 0, 1, 0, 0]
+
+# c = [0, 0]
+
+def jumpingOnClouds(c):
+  jumps = 0
+  i = 0
+  
+  while i < (len(c)-1):
+    if(i < (len(c)-2) and c[i+2] == 0):
+      i +=1
+    
+    i +=1    
+    jumps +=1
+
+  return jumps
+
+
+print(jumpingOnClouds(c))
